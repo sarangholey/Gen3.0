aca5e20576d0414b9751694e29e364f928f505a6
sarangholey <sarangholey@gmail.com>
2020-06-06 14:03:42 +0530

    Changed value of Implicit,explicit and pageload timeout 20 to 30 Seconds. With attribute parallel = "classes" in regression.xml

--- src/test/java/com/qa/amazonind/util/AppConstants.java
+++ src/test/java/com/qa/amazonind/util/AppConstants.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
  */
 public class AppConstants {
 
-	public static int DEFAULT_EXPLICIT_TIME_OUT = 20;
+	public static int DEFAULT_EXPLICIT_TIME_OUT = 30;
 
 	public static final String LANDING_PAGE_TITLE = "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
 
